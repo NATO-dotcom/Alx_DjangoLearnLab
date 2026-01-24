b36b0c5c680d8d192b0a625bf81dfffc7bab54bb
NATO-dotcom <dnaliaka365@gmail.com>
2026-01-24 23:00:17 +0300

    Modified my code and tried to test working

--- LibraryProject/LibraryProject/urls.py
+++ LibraryProject/LibraryProject/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # 1. Added 'include' here
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # 2. This routes any request to the relationship_app/urls.py
+    path('', include('relationship_app.urls')), 
 ]
